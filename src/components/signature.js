6405917e4d16cca0432b7ac52c28d8d132246b8b
Rafal Wilinski <raf.wilinski@gmail.com>
2020-06-05 11:35:02 +0200

    Change signature a bit

--- src/components/signature.js
+++ src/components/signature.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { ColorExternalLink } from '../theme'
 
 const Signature = () => (
-  <p style={{ fontSize: '0.85em', marginLeft: '0.5em' }}>
+  <p style={{ fontSize: '0.85em' }}>
+    Thanks for reading!
+    <br />
+    <br />
     -- Written by{' '}
     <ColorExternalLink href="https://twitter.com/RafalWilinski">
       Rafal Wilinski
